bc2f63317a87f4ebd54c769e8a6fedcbefc7619c
minkhantzaw <minkhantzaw@g.com>
2024-02-09 14:18:05 +0630

    first commit

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SnippetNotFound = () => {
+  return (
+    <div>
+      <h1 className="text-xl bold">Sorry we couldn't find that snippet </h1>
+    </div>
+  );
+};
+
+export default SnippetNotFound;
---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,59 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+const SnippetCreatePage = () => {
+  async function createSnippet(formData: FormData) {
+    // This need to be server action
+    "use server";
+
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // Redirect the user back to the root route
+    redirect("/");
+  }
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet!</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className=" rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SnippetCreatePage;
